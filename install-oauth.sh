af493690121e40c63e839520c8884dd3b5dfbb90
Cristian Calara <cristian@derikon.com>
2019-03-06 09:20:20 +0200

    Add install oauth script

--- install-oauth.sh
+++ install-oauth.sh
@@ -1,6 +1,7 @@
 sudo apt-get -y install gcc make autoconf libc-dev pkg-config
 sudo pecl install oauth
 
-sudo bash -c "echo extension=oauth.so > /etc/php/7.2/fpm/conf.d/oauth.ini"
-sudo bash -c "echo extension=oauth.so > /etc/php/7.2/cli/conf.d/oauth.ini"
+sudo bash -c "echo extension=oauth.so > /etc/php/7.2/mods-available/oauth.ini"
+sudo ln -s /etc/php/7.2/mods-available/oauth.ini /etc/php/7.2/fpm/conf.d/30-oauth.ini
+sudo ln -s /etc/php/7.2/mods-available/oauth.ini /etc/php/7.2/cli/conf.d/30-oauth.ini
 sudo service php7.2-fpm restart
